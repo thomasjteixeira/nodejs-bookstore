c606f55a9dce26f31d24f595b233d7194afe2f34
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-09-15 15:35:34 -0300

    feat: patch

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const books = [
   },
 ];
 
+// Função para buscar o livro pelo id
 function searchBook(id) {
   return books.findIndex((book) => book.id === id);
 }
@@ -39,6 +40,16 @@ app.get("/books/:id", (req, res) => {
     res.status(404).json("Livro não encontrado");
   } else {
     res.status(200).json(books[index]);
+  }
+});
+
+app.patch("/books/:id", (req, res) => {
+  const index = searchBook(Number(req.params.id));
+  if (index === -1) {
+    res.status(404).json("Livro não encontrado");
+  } else {
+    books[index].title = req.body.title;
+    res.status(200).json(books);
   }
 });
 
