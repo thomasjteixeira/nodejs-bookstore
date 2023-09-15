ee4dc806166d66b96212866d89bdf772d6ea2da0
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-09-15 15:00:28 -0300

    feat: added books arrays

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const books = [
+  {
+    id: 1,
+    title: "Harry Potter",
+  },
+  {
+    id: 2,
+    title: "Senhor dos Anéis",
+  },
+];
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!!!");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
 });
 
 export default app;
