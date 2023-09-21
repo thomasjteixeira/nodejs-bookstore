6d4863be60c67209b13cd0147352ca11c5cd6b76
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-09-21 17:40:27 -0300

    fix: envs

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// dotenv, que é usado para carregar variáveis de ambiente a partir de um arquivo .env.
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
@@ -5,4 +7,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta 3000");
 });
-
